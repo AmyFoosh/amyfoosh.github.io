3c41d9c17b728884bc635fd46d3f25a6cf26ee2a
AmyFoosh <67493553+AmyFoosh@users.noreply.github.com>
2022-01-06 22:24:59 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,15 +1,13 @@
+let stage = document.getElementById("canvas");
+let ctx = stage.getContext("2d");
+
+ctx.canvas.width  = window.innerWidth * 0.99;
+ctx.canvas.height = window.innerHeight * 0.99;
+
+let avatar = new Image();
+avatar.src = "img/avatar.png";
+
 function init() {
 
-   let stage = document.getElementById("canvas");
-   let ctx = stage.getContext("2d");
-
-   let avatar = new Image();
-   avatar.src = "./img/avatar.png";
-
-   stage.width = 300;
-   stage.height = 300;
    
-   alert("yes");
-
-   ctx.drawImage(avatar, stage.width / 2, stage.height / 2);
 }
