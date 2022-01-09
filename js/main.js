4283d6f6a6cc05ea1ea27fe666705f75de7f4efe
AmyFoosh <67493553+AmyFoosh@users.noreply.github.com>
2022-01-09 02:07:37 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,20 @@
 let stage = document.getElementById("canvas");
 let ctx = stage.getContext("2d");
 
+let fps = 1000 / 60;
+
+// Resize canvas to match screen size.
 ctx.canvas.width = window.innerWidth * 0.99;
 ctx.canvas.height = window.innerHeight * 0.99;
 
 let avatar = new Image();
 avatar.src = "img/avatar.png";
+
+setInterval(gameLoop, fps);
+
+function gameLoop() {
+
+}
 
 function init() {
 
